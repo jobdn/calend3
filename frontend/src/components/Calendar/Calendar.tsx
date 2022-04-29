1e5ad0732b8d8d1929d34baa7b7e8dded0d429cf
Danila Pisarev <jobdan@bk.ru>
2022-04-29 16:22:03 +0300

    feat: add real Calendar component

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -1,7 +1,47 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { Scheduler } from "devextreme-react/scheduler";
+
+import "devextreme/dist/css/dx.light.css";
+import { AppointmentAddedEvent } from "devextreme/ui/scheduler";
+
+interface IAppointment {
+  text: string;
+  startDate: Date;
+  endDate: Date;
+}
 
 const Calendar: FC = () => {
-  return <div id="calendar">Calendar</div>;
+  const [appointments, setAppointments] = useState<IAppointment[]>([
+    {
+      text: "Call",
+      startDate: new Date(2022, 3, 29, 16, 45),
+      endDate: new Date(2022, 3, 29, 17, 45),
+    },
+  ]);
+
+  const onAddAppointment = (e: AppointmentAddedEvent) => {
+    // TODO: here calend3 will be called
+    console.log(e.appointmentData);
+  };
+
+  return (
+    <Scheduler
+      id="scheduler"
+      defaultCurrentView={"week"}
+      dataSource={appointments}
+      views={[
+        { type: "day", startDayHour: 10, endDayHour: 18 },
+        {
+          type: "week",
+          startDayHour: 10,
+          endDayHour: 18,
+        },
+        "month",
+      ]}
+      onAppointmentAdded={onAddAppointment}
+      adaptivityEnabled={true}
+    ></Scheduler>
+  );
 };
 
 export default Calendar;
